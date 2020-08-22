9422d3df476fd4943f654f4623849eeece6c43d9
hiransh116 <hiranshanand116@gmail.com>
2020-08-22 17:55:28 +0530

    'platform'

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var chain;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -35,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    chain= new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -61,4 +61,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain.display();
 }
